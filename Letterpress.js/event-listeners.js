780067d896a3f8e710b626a9f1a94e7008a4830c
neutrino <jrusev@gmail.com>
2014-06-07 14:11:44 +0300

    separated event listeners

--- Letterpress.js/event-listeners.js
+++ Letterpress.js/event-listeners.js
@@ -38,7 +38,6 @@ function mouseUpListener(evt) {
     window.removeEventListener("mouseup", mouseUpListener, false);
     if (isDragging) {
         isDragging = false;
-        dragTile.isMoving = false;
         // Make the tile return to its intial position
         dragTile.targetPosX = dragTile.initalX;
         dragTile.targetPosY = dragTile.initalY;
@@ -77,4 +76,26 @@ function getMousePos(canvas, evt) {
         X: (evt.clientX - bRect.left) * (canvas.width / bRect.width),
         Y: (evt.clientY - bRect.top) * (canvas.height / bRect.height)
     };
+}
+
+
+
+ // Runs while the timer is ticking
+function onTimerTick() {
+    // The next variable controls the lag in the tile movement (from 0 to 1)
+    var easeAmount = 0.2;
+    // Update the moving tile position
+    dragTile.X += easeAmount * (dragTile.targetPosX - dragTile.X);
+    dragTile.Y += easeAmount * (dragTile.targetPosY - dragTile.Y);
+
+    // Stop the timer when the target position is reached (close enough)
+    if ((!isDragging) && (Math.abs(dragTile.X - dragTile.targetPosX) < 0.1) && (Math.abs(dragTile.Y - dragTile.targetPosY) < 0.1)) {
+        // Snap the tile to its final position
+        dragTile.X = dragTile.targetPosX;
+        dragTile.Y = dragTile.targetPosY;
+        dragTile.isMoving = false;
+        // Stop timer:
+        clearInterval(timer);
+    }
+    drawScreen();
 }
