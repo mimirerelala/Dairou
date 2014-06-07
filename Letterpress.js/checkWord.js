5feb76fa090639081930238426394f8e1984bf2c
krumov <krasin.krumov@gmail.com>
2014-06-07 16:45:44 +0300

    resized canvas and words and added small funcs

---
+++ Letterpress.js/checkWord.js
@@ -0,0 +1,14 @@
+﻿function checkIfWordRepeats(word) {
+
+    var words = ['cats', 'dog', 'pigs', 'babysitter'];
+
+    for (var i = 0; i < words.length; i++) {
+
+        var answer = words[i].indexOf(word);
+
+        if (answer !== -1) {
+            return true;
+        }
+    }
+    return false;
+}
