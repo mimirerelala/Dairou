507d91d62a00f7a07fffc80468fdc9f8d46b9d4f
neutrino <jrusev@gmail.com>
2014-06-18 15:59:02 +0300

    added turn indication

--- Letterpress.js/scripts/main.js
+++ Letterpress.js/scripts/main.js
@@ -27,13 +27,15 @@ var colors = {
 
 function main() {
 
+    gamePlayers = initializePlayers();
+    matrixColors = initializeColorMatrix();
+
     prepareCanvas();
     prepareBoard();
     drawScreen();
     prepareSvg(document.documentElement.clientWidth / 2, canvas.width, canvas.height);
 
-    gamePlayers = initializePlayers();
-    matrixColors = initializeColorMatrix();
+
 
     canvas.addEventListener("mousedown", mouseDownListener, false);
 }
@@ -54,19 +56,18 @@ function prepareBoard() {
 
     tiles = makeTiles(boardLetters);
     wordHolder = new WordHolder(0, canvas.height / 20);
-	submitButton = new Button('Submit', 30, 'rgb(255, 67, 47)', 'rgb(240, 239, 236)', 645, -3, 40);
-	clearButton = new Button('Clear', 30, 'rgb(0, 162, 255)', 'rgb(240, 239, 236)', 20, -3, 40);
+    submitButton = new Button('Submit', 30, 'rgb(255, 67, 47)', 'rgb(240, 239, 236)', 645, -3, 40);
+    clearButton = new Button('Clear', 30, 'rgb(0, 162, 255)', 'rgb(240, 239, 236)', 20, -3, 40);
 }
 
 // Renders the canvas to screen
 function drawScreen() {
-  
+
     context.fillStyle = 'rgb(240, 239, 236)';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
-    context.save();    
     drawScores();
-    context.restore();
+    drawTriangle();
 
     submitButton.draw(context);
     clearButton.draw(context);
@@ -76,17 +77,46 @@ function drawScreen() {
 
 }
 
+function drawTriangle() {
+    var color = redTurn ? colors.darkRed : colors.darkBlue;
+    var cx = redTurn ? 80 : 710;
+    var cy = 315;
+    var side = 15;
+    var h = side * (Math.sqrt(3) / 2);
+
+    context.save();
+    context.fillStyle = color;
+
+    context.translate(cx, cy);
+    context.beginPath();
+    context.moveTo(0, h / 2);
+    context.lineTo(-side / 2, -h / 2);
+    context.lineTo(side / 2, -h / 2);
+    context.lineTo(0, h / 2);
+    context.closePath();
+
+    context.lineWidth = Math.max(~~(h / 5), 5);
+    context.lineJoin = "round";
+    context.strokeStyle = color;
+    context.stroke();
+
+    context.fill();
+    context.restore();
+}
+
 function drawScores() {
+    context.save();
     var redScoreAsString = redScore.toString();
     var blueScoreAsString = blueScore.toString();
 
     context.textAlign = 'center';
     context.font = 'bold 68px Calibri';
-	context.fillStyle = 'rgb(255, 67, 47)';
+    context.fillStyle = 'rgb(255, 67, 47)';
     context.fillText(redScoreAsString, 80, 380); //canvas.width * 0.4, canvas.heigh * 0.1);
 
-	context.fillStyle = 'rgb(0, 162, 255)';
+    context.fillStyle = 'rgb(0, 162, 255)';
     context.fillText(blueScoreAsString, 710, 380); //canvas.width * 0.6, canvas.heigh * 0.1);
+    context.restore();
 }
 
 // Returns an array of Tile objects
@@ -135,8 +165,10 @@ function addWord(wordToSubmit) {
 }
 
 function initializePlayers() {
-    Player1 = new Player("Zhivko", colors.red, colors.darkRed);
-    Player2 = new Player("Villy", colors.blue, colors.darkBlue);
+    Player1 = new Player("Villy", colors.blue, colors.darkBlue);
+    Player2 = new Player("Zhivko", colors.red, colors.darkRed);
+    if (Player1.darkColor === colors.darkRed)
+        redTurn = true;
     var gamePlayers = [];
     gamePlayers[0] = Player1;
     gamePlayers[1] = Player2;
@@ -147,6 +179,7 @@ function switchPlayers(playersIn) {
     playersIn[2] = playersIn[0];
     playersIn[0] = playersIn[1];
     playersIn[1] = playersIn[2];
+    redTurn = !redTurn;
     return playersIn;
 }
 
